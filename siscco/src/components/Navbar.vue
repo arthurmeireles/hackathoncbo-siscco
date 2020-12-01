a8c6a0286f95f0dcd66dcad98d7e4a1a325f3b4c
inaldomarinho <inaldomarinhoqueirozneto@gmail.com>
2020-11-30 23:12:08 -0300

    Fazendo modificações na página principal

--- siscco/src/components/Navbar.vue
+++ siscco/src/components/Navbar.vue
@@ -1,44 +1,34 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar toggleable="lg border-bottom border-dark">
+            <b-navbar-brand href="#">
+                <div class="h2 mb-0">
+                    <b-icon-list style="color: #3086fa;" ></b-icon-list>
+                </div>
+            </b-navbar-brand>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
+            <img width="160" class="m-auto" src="@/assets/img/logo-medium.svg" alt="">
 
             <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                </b-nav-form>
-
-                <b-nav-item-dropdown text="Lang" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template #button-content>
-                    <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                </b-nav-item-dropdown>
+            <b-navbar-nav class="ml-auto h2 mb-0 nav-user-icon">
+                <b-avatar icon="person-fill"></b-avatar>
             </b-navbar-nav>
-            </b-collapse>
         </b-navbar>
     </div>
 </template>
 
+<style>
+.navbar-dark .navbar-nav .nav-link{
+    color:black!important
+}
+
+.nav-user-icon {
+    border-radius: 50%;
+    /* flex-shrink: 0; */
+    background: var(--gray);
+}
+</style>
+ 
 <script>
 export default {
     name: 'Navbar'
--- siscco/src/components/PaginaInicial/PaginaInicial.vue
+++ siscco/src/components/PaginaInicial/PaginaInicial.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>
-        
-        
-
-    </div>
+    <b-container class='border-left border-right border-dark'>
+        <b-col class="h-100">
+            <b-container class="w-100 border-bottom border-dark">
+                <div class="pt-3 pb-3">
+                    <b-row>
+                        <b-avatar icon="person-fill"></b-avatar>
+                        <b-col>
+                            <b-span class="ml-2" style="color: #FFF;">Usuário</b-span>
+                            <div class="w-100"></div>
+                            <div class="rounded-circle" style="color: #FFF; width: 5px; height: 5px;" ></div>
+                        </b-col>
+                    </b-row>
+                    <h2 class="ml-5 pt-2" style="color: #FFF;">Qual a CID correta Infarto agudo do miocárdio?</h2>
+                </div>
+            </b-container>
+        </b-col>
+    </b-container>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
