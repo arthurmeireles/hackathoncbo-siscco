8aa9f6cfb475d7588deff25f10503d20afb725e4
Beatriz Soares <beatriz.souza@lais.huol.ufrn.br>
2020-12-03 21:46:54 -0300

    Pagina de cursos

--- siscco/src/components/cursos/cursos.vue
+++ siscco/src/components/cursos/cursos.vue
@@ -1,7 +1,42 @@
 <template>
     <div>
         <div class="card text-white my-border-radius mt-5">
+            <div class="card-body pb-2 ">
+                <div class="row col">
+                    <div class="ml-3">
+                            <h6 class="card-title" >
+                                Curso de Capacitação em Codificação de Causa Básica do Óbito – CID-10 - Versão 2
+                            </h6>
 
+                        <div class="d-flex mb-2">
+                            <h6 class="card-subtitle text-muted mr-1">
+                                <i class="medalha-ouro fas fa-medal"> </i> 
+                                <a href="https://avasus.ufrn.br/local/avasplugin/cursos/curso.php?id=382">
+                                Acesse para ganhar Medalhas</a>
+                                
+                            
+                            </h6>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="ml-3">
+                            <h6 class="card-title" >
+                                Módulo a Distância do Curso de Capacitação em Codificação de Causa Básica do Óbito – CID-10
+                            </h6>
+
+                        <div class="d-flex mb-2">
+                            <h6 class="card-subtitle text-muted mr-1">
+                                <i class="medalha-ouro fas fa-medal"> </i> 
+                                <a href="https://avasus.ufrn.br/local/avasplugin/cursos/curso.php?id=231">
+                                Acesse para ganhar Medalhas
+                                </a>
+                                
+                            
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+             </div>
         </div>
     </div>
 </template>
